28df21f1cbc8b48870d3424604215269a1fe216a
esm33 <esm33@njit.edu>
2025-05-11 11:08:22 -0400

    Adding the feedback form for the admin + HTML/CSS pages
     feedback report for administrators.

--- e_paradise/e_p/urls.py
+++ e_paradise/e_p/urls.py
@@ -1,10 +1,26 @@
 from django.urls import path
-from django.urls import include
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='homepage'),
+    path('Login/', views.Login, name='Login'),
+    path('Signup/', views.Signup, name='Signup'),
+    path('ExplorePlatforms/', views.explore_platforms, name='Explore'),
+    path('explore/<int:pk>/reviews/', views.explore_reviews, name='explore_reviews'),
+    path('admin-feedback-report/', views.feedback_report, name='feedback_report')
+]
+
+#path('', views.index, name='index'),
+#path('about/', views.about, name='about'),
+#path('contact/', views.contact, name='contact'),
+#path('products/', views.products, name='products'),
+#path('services/', views.services, name='services'),
+#path('blog/', views.blog, name='blog'),
+    
+'''
+    path('feedback-report/', views.feedback_report, name='feedback_report'),
+    
     path('test', views.index),
-    path('e_p/<str:any_name>', views.base, name="base"), # name paremeter: A unique name to refer to the route elsewhere (like in templates or redirects)
     #path("homepage/", views.home, name='homepage'),
     path('', views.home, name='homepage'),
     path('TempProductPage/', views.ProductTemp, name='TempProductPage'),
@@ -13,12 +29,5 @@ urlpatterns = [
     #path('ExplorePlatforms/', views.Explore, name="Explore"),
     path('ExplorePlatforms/', views.explore_platforms, name="Explore"),
     path('explore/<int:pk>/reviews/', views.explore_reviews, name='explore_reviews'),
-    
-    ]
-
-#path('', views.index, name='index'),
-#path('about/', views.about, name='about'),
-#path('contact/', views.contact, name='contact'),
-#path('products/', views.products, name='products'),
-#path('services/', views.services, name='services'),
-#path('blog/', views.blog, name='blog'),
+    #path('e_p/<str:any_name>', views.base, name="base") # name paremeter: A unique name to refer to the route elsewhere (like in templates or redirects)
+'''
